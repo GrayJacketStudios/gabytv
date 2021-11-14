a69fe9eda9982f046aff1cb0da03105e722ed780
Sebastian Cristi <grayjacketstudios@gmail.com>
2021-11-14 14:59:06 -0300

    Fixed the tests and functionality for the search function #23

--- src/utils/contentHelper.ts
+++ src/utils/contentHelper.ts
@@ -5,9 +5,9 @@ export const getContentByType = (content: IContent[], type: Type) => {
 };
 
 export const getContentByTitle = (content: IContent[], title: string) => {
-    return content.filter(item => item.title.includes(title));
+    return content.filter(item => item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()));
 };
 
 export const getContentBySynopsis = (content: IContent[], synopsis: string) => {
-    return content.filter(item => item.synopsis.includes(synopsis));
+    return content.filter(item => item.synopsis.toLocaleLowerCase().includes(synopsis.toLocaleLowerCase()));
 }
